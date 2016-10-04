f030f1381bfb416ceb28cf73e9a697e5ae7625df
Drew Olbrich <drew.olbrich@gmail.com>
2016-10-03 19:05:59 -0700

    Add Bullet.

--- Game1/GameScene.swift
+++ Game1/GameScene.swift
@@ -11,6 +11,8 @@ class GameScene: SKScene {
     var lastTouchPosition = CGPoint(x: 0.0, y: 0.0)
 
     var shootingTimer: Timer?
+
+    let playerballImageName =  "playerball"
     
     required init?(coder decoder: NSCoder) {
         super.init(coder: decoder)
@@ -69,7 +71,7 @@ class GameScene: SKScene {
 
         let touchPosition = convertTouchLocationToScene(touch: touches.first!)
         aimSpaceship(at: touchPosition)
-        shootBullet(at: touchPosition)
+        shootBullet(scene: self, from: gunNode.position, to: touchPosition, imageNamed: playerballImageName)
         
         lastTouchPosition = touchPosition
         startShooting()
@@ -132,7 +134,7 @@ class GameScene: SKScene {
         assert(shootingTimer == nil, "Shooting timer is already defined.")
         
         shootingTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { (Timer) in
-            self.shootBullet(at: self.lastTouchPosition)
+            shootBullet(scene: self, from: self.gunNode.position, to: self.lastTouchPosition, imageNamed: self.playerballImageName)
         })
     }
     
@@ -140,34 +142,5 @@ class GameScene: SKScene {
         shootingTimer?.invalidate()
         shootingTimer = nil
     }
-    
-    func shootBullet(at position: CGPoint) {
-        let bulletNode = SKSpriteNode(imageNamed: "playerball")
-        bulletNode.setScale(1.5)
-        self.addChild(bulletNode)
-        bulletNode.position = gunNode.position
-
-        bulletNode.zPosition = 75.0
-
-        // The direction we should fire the bullet.
-        var direction = CGPoint(x: position.x - gunNode.position.x, y: position.y - gunNode.position.y)
-        // Set the length of the direction vector to 1.
-        let length = sqrt(direction.x*direction.x + direction.y*direction.y)
-        direction.x /= length
-        direction.y /= length
         
-        // The distance that the bullet will travel before stopping. This is 50% more than
-        // the width of the screen so it'll always be a point off the edge of the screen.
-        let distance = CGFloat(screenWidth*1.5)
-        
-        // The end point of the bullet along its path before we remove it from the scene.
-        let endPoint = CGPoint(x: gunNode.position.x + distance*direction.x, y: gunNode.position.y + distance*direction.y)
-        
-        // Move the bullet.
-        let moveAction = SKAction.move(to: endPoint, duration: 1.45)
-        bulletNode.run(moveAction) { 
-            bulletNode.removeFromParent()
-        }
-    }
-    
 }
